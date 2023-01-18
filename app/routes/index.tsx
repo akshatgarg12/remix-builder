fd228d44c753caf1b47d6a0251fe7935d9633ed1
akshatgarg12 <akshatarungarg78@gmail.com>
2023-01-18 18:06:04 +0530

    added ways to handle ts error

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,7 @@ import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node"; // or cloudflare/deno
 import { useLoaderData } from "@remix-run/react";
 import {builder, BuilderComponent, Builder} from "@builder.io/react";
+import type {BuilderContent} from "@builder.io/sdk"
 import Counter from "~/components/Counter";
 builder.init("8335d18816304315aebeb7e9532281ce")
 
@@ -9,19 +10,26 @@ Builder.registerComponent(Counter, {
   name : 'Counter'
 })
 
+// remix-typedjson
+// as unknown as BuilderContent
+// useLoaderData<LoaderData>()
+
 export const loader:LoaderFunction = async () => {
   const content = await builder.get("page", {
           options: { includeUnpublished: true },
           userAttributes: { urlPath: '/remix' },
         }).promise()
-  return json(content)
+  return json(content) 
 };
 
 export default function Index() {
-  const page = useLoaderData<typeof loader>()
+  const page = useLoaderData() as unknown as BuilderContent
   return (
     <div>
       <BuilderComponent model="page" content={page} />
+      {/* {
+        JSON.stringify(page)
+      } */}
     </div>
   );
 }
